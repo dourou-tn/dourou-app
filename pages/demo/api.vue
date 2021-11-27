aae68de87ef6e6589e88aedce4d98201daf6baf5
Jrb <jrb.youssef@gmail.com>
2021-11-27 11:50:02 +0100

    create demo api

---
+++ pages/demo/api.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="getData">Click</button>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoApi',
+  data () {
+    return {
+      data: '',
+    }
+  },
+  methods: {
+    async getData () {
+      const json = await fetch('/api/getJSON');
+      this.data = await json.json();
+    }
+  },
+}
+</script>
